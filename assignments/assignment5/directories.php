f63459c192aba9d4f5bee4e16ef86f5534aa6ddc
belthix <112828565+belthix@users.noreply.github.com>
2022-10-24 23:55:12 -0400

    Removed refernece code

--- assignments/assignment5/directories.php
+++ assignments/assignment5/directories.php
@@ -37,35 +37,3 @@
     }
 
 ?>
-
-<?php
-
-    class NameHandler {
-        
-        private static $_base_path = '/dirs/';
-        private $_msg;
-
-        public function __construct()
-        {
-            #Exit if no input
-            if (count($_POST) || (isset($_POST['folderName']) || isset($_POST['fileContents']))) {
-                $this->_names = [];
-            } else {
-                $this->_names = isset($_POST['nameList']) ? explode("\n", $_POST['nameList']) : [];
-            
-                if (isset($_POST['newName'])) {
-                    $newName = explode(' ', $_POST['newName'], 2);
-
-                    #Skip if incorrect syntax
-                    if (count($newName) == 2) {
-                        array_push($this->_names, "{$newName[1]}, {$newName[0]}");
-                        natcasesort($this->_names);
-                    }
-                }
-            }
-        }
-
-        public function getMsg() { return implode("\n", $this->_names); }
-    }
-
-?>
